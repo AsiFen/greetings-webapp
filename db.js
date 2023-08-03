10e3ae39e5930f0ce50dbc42821c52f080cd5dd1
Asisipho <missfente@gmail.com>
2023-08-04 00:41:12 +0200

    vscode gone rougge

--- db.js
+++ db.js
@@ -12,4 +12,26 @@ const pgp = pgPromise();
 const db = pgp(connectPostgres);
 
 //export the database
+// Insert a new greeting count into the database
+async function insertGreetingCount(name, count) {
+    try {
+        await db.none('INSERT INTO greeting_counts (name, count) VALUES ($1, $2) ON CONFLICT (name) DO NOTHING', [name, count]);
+    } catch (error) {
+        console.error('Error inserting greeting count into database:', error);
+        throw error;
+    }
+}
+
+// Update an existing greeting count in the database
+async function updateGreetingCount(name) {
+    try {
+        await db.none('UPDATE greeting_counts SET count = count + 1 WHERE name = $1', [name]);
+    } catch (error) {
+        console.error('Error updating greeting count in database:', error);
+        throw error;
+    }
+}
+
 export default db;
+
+export { insertGreetingCount, updateGreetingCount };
--- test/greet.test.js
+++ test/greet.test.js
@@ -1,17 +1,43 @@
-// import assert from assert;
-// import GreetingsExercise from "../greet";
-// import db from "../db";
-import pgPromise from "pg-promise";
+// test/sqlCommands.test.js
 
-const connectPostgres = {
-    connectionString: 'postgres://asifen:mBANIlUToXYk6YQhwlqX3Um62XLUaZmx@dpg-cj4l5os5kgrc739mq7l0-a.oregon-postgres.render.com/users_db_rymv',
-    ssl: { rejectUnauthorized: false }, // this line to enable SSL/TLS with self-signed certificates
+import { insertGreetingCount, updateGreetingCount } from '../db.js';
+import assert from 'assert';
+import pgPromise from 'pg-promise';
+
+const connectPostgresTest = {
+    // connectionString: 'postgres://asifen:mBANIlUToXYk6YQhwlqX3Um62XLUaZmx@dpg-cj4l5os5kgrc739mq7l0-a.oregon-postgres.render.com/users_db_rymv',
+    ssl: false, // If your test database doesn't use SSL
 };
 
-// Create an instance of the promise
-const pgp = pgPromise();
+const pgpTest = pgPromise();
+const dbTest = pgpTest(connectPostgresTest);
+// test/asyncFunctions.test.js
 
-// Link promise module with my postgres URL
-const db = pgp(connectPostgres);
+describe('Async/Await Functions Tests', () => {
+    it('should insert a new greeting count into the database', async () => {
+        // Use a mock database connection for testing
+        // For example, you can use the 'pg-promise' mock: https://github.com/vitaly-t/pg-promise/blob/master/test/mock/db.js
 
+        // Call the insertGreetingCount function with the mock database connection
+        await insertGreetingCount('John', 0, dbTest);
 
+        // Check if the greeting count was inserted correctly
+        assert.strictEqual(dbTest.executedQueries[0].text, 'INSERT INTO greeting_counts (name, count) VALUES ($1, $2) ON CONFLICT (name) DO NOTHING');
+        assert.deepStrictEqual(dbTest.executedQueries[0].values, ['John', 0]);
+    });
+
+    it('should update an existing greeting count in the database', async () => {
+        // Use a mock database connection for testing
+        // For example, you can use the 'pg-promise' mock: https://github.com/vitaly-t/pg-promise/blob/master/test/mock/db.js
+
+        // Call the updateGreetingCount function with the mock database connection
+        await updateGreetingCount('John', dbTest);
+
+        // Check if the greeting count was updated correctly
+        assert.strictEqual(dbTest.executedQueries[0].text, 'UPDATE greeting_counts SET count = count + 1 WHERE name = $1');
+        assert.deepStrictEqual(dbTest.executedQueries[0].values, ['John']);
+    });
+
+    // Add more test cases for the async/await functions...
+
+});
