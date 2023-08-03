94776e429e63bcf1508ee41c68a8caa85410531c
Asisipho <missfente@gmail.com>
2023-08-03 23:09:50 +0200

    fails when not actual tests are written

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,22 +1,17 @@
-import assert from assert;
-import GreetingsExercise from "../greet";
+// import assert from assert;
+// import GreetingsExercise from "../greet";
+// import db from "../db";
 import pgPromise from "pg-promise";
 
-const connectPromise = process.env.DATABASE_URL || 'postgres://asifen:mBANIlUToXYk6YQhwlqX3Um62XLUaZmx@dpg-cj4l5os5kgrc739mq7l0-a.oregon-postgres.render.com/users_db_rymv';
+const connectPostgres = {
+    connectionString: 'postgres://asifen:mBANIlUToXYk6YQhwlqX3Um62XLUaZmx@dpg-cj4l5os5kgrc739mq7l0-a.oregon-postgres.render.com/users_db_rymv',
+    ssl: { rejectUnauthorized: false }, // this line to enable SSL/TLS with self-signed certificates
+};
 
+// Create an instance of the promise
 const pgp = pgPromise();
 
-const db = pgp(connectPromise);
+// Link promise module with my postgres URL
+const db = pgp(connectPostgres);
 
-describe("The basic database web app", function () {
-    beforeEach(async function () {
-        try {
-            // clean the tables before each test run
-            await db.none("TRUNCATE TABLE products RESTART IDENTITY CASCADE;");
-            await db.none("TRUNCATE TABLE categories RESTART IDENTITY CASCADE;");
-        } catch (err) {
-            console.log(err);
-            throw err;
-        }
-    });
-});
+
