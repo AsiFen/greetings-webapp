4a216a3c66da84ee7031d3279aaf22b6f6e1b626
Asisipho <missfente@gmail.com>
2023-08-03 23:01:33 +0200

    installed mocha

--- index.js
+++ index.js
@@ -41,6 +41,19 @@ app.use(bodyParser.json())
 //built-in static middleware from ExpressJS to use static resources
 app.use(express.static('public'))
 
+
+// Test route to select data from the 'greetings' table
+app.get('/test', async (req, res) => {
+    try {
+      // Replace 'greetings' with the name of your table
+      const result = await db.any('SELECT * FROM greeting_counts');
+      res.json(result);
+    } catch (error) {
+      console.error('Error executing query:', error);
+      res.status(500).json({ error: 'Failed to retrieve data from the database.' });
+    }
+  });
+
 //root directory
 //displays greetings index page 
 app.get('/', (req, res) => {
--- test/greet.test.js
+++ test/greet.test.js
@@ -1,11 +1,22 @@
-// Test route to select data from the 'greetings' table
-app.get('/test', async (req, res) => {
-    try {
-      // Replace 'greetings' with the name of your table
-      const result = await db.any('SELECT * FROM greeting_counts');
-      res.json(result);
-    } catch (error) {
-      console.error('Error executing query:', error);
-      res.status(500).json({ error: 'Failed to retrieve data from the database.' });
-    }
-  });
+import assert from assert;
+import GreetingsExercise from "../greet";
+import pgPromise from "pg-promise";
+
+const connectPromise = process.env.DATABASE_URL || 'postgres://asifen:mBANIlUToXYk6YQhwlqX3Um62XLUaZmx@dpg-cj4l5os5kgrc739mq7l0-a.oregon-postgres.render.com/users_db_rymv';
+
+const pgp = pgPromise();
+
+const db = pgp(connectPromise);
+
+describe("The basic database web app", function () {
+    beforeEach(async function () {
+        try {
+            // clean the tables before each test run
+            await db.none("TRUNCATE TABLE products RESTART IDENTITY CASCADE;");
+            await db.none("TRUNCATE TABLE categories RESTART IDENTITY CASCADE;");
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    });
+});
