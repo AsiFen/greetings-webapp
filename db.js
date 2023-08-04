b7a7ff1be8cf239e29c39ed5c54102325abae3f7
Asisipho <missfente@gmail.com>
2023-08-05 01:23:01 +0200

    npm test

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import pgPromise from 'pg-promise';
 
 const connectPromise = {
-    
-  connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }, // this line to enable SSL/TLS with self-signed certificates
+
+  connectionString: process.env.DATABASE_URL
+    // ssl: { rejectUnauthorized: false }, // this line to enable SSL/TLS with self-signed certificates
 };
 
 const pgp = pgPromise();
