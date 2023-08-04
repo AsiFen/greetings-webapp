6452c9882e49c57fc60920fde90e505c654af3cc
Asisipho <missfente@gmail.com>
2023-08-04 22:13:41 +0200

    snytax error was assert, did not have qoutations

---
+++ dotenv.js
@@ -0,0 +1,11 @@
+///checkigng if env is set or not 
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if (process.env.DATABASE_URL) {
+  console.log('DATABASE_URL is set:');
+} else {
+  console.log('DATABASE_URL is not set.');
+}
--- test/greet.test.js
+++ test/greet.test.js
@@ -1,44 +1,26 @@
-import assert from assert;
-import GreetingsExercise from "../greet";
+import assert from 'assert';
+import GreetingsExercise from "../greet.js";
 import pgPromise from "pg-promise";
+import dotenv from 'dotenv';
 
-const connectPromise = {
-    connectionString: 'postgres://asifen:mBANIlUToXYk6YQhwlqX3Um62XLUaZmx@dpg-cj4l5os5kgrc739mq7l0-a.oregon-postgres.render.com/users_db_rymv',
-    ssl: { rejectUnauthorized: false }, // this line to enable SSL/TLS with self-signed certificates
-};
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL
+
 const pgp = pgPromise();
 
-const db = pgp(connectPromise);
+const db = pgp(connectionString);
 
-describe('Database Tests', () => {
-  before(async () => {   
-    //  this.timeout(3000);
-
-    // Run any database setup before the tests start
-    await db.none('CREATE TABLE greeting_counts(name VARCHAR(50) PRIMARY KEY, count INT NOT NULL)');
-  });
-
-  beforeEach(async () => {
-    this.timeout(3000);
-
-    // Run any database setup before each test
-    await db.none('TRUNCATE TABLE greeting_counts RESTART IDENTITY');
-  });
+describe('Database Tests For Greetings WebApp', () => {
 
   it('should insert a new greeting count into the database', async () => {
-    this.timeout(3000);
+    this.timeout(10000);
     const greet_instance = GreetingsExercise(db);
     greet_instance.makeGreet('Thando', 'english');
     greet_instance.countGreet();
 
-    const result = await db.one('SELECT count FROM greeting_counts WHERE name = $1', ['Thando']);
+    const result = await db.any('SELECT * FROM greeting_counts WHERE name = $1', ['Thando']);
+    console.log(result);
     assert.strictEqual(result.count, 1);
   });
-});
-
-
-after(async () => {
-    // this.timeout(3000);
-
-  await db.none('DROP TABLE greeting_counts');
 });
