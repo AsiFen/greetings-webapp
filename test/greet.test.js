cfd9d752e16a545b2f6310cd2f36abf6c29a1192
Asisipho <missfente@gmail.com>
2023-08-08 16:05:05 +0200

    set db_url to db on render

--- test/greet.test.js
+++ test/greet.test.js
@@ -3,7 +3,7 @@ import Greetings from '../db/db-logic.js';
 
 import pgPromise from 'pg-promise';
 
-const connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/users";
+const connectionString = process.env.DATABASE_URL;
 
 const db = pgPromise()(connectionString);
 
